50e3e8fa38de7cc5dd32e250d2f4b8c3ec4071da
CaNavas <navasochoa@hotmail.com>
2023-10-15 11:07:58 -0500

    css

---
+++ proyectoConApi/interactivo.js
@@ -0,0 +1,8 @@
+const nombre = document.querySelector('#nombre')
+const enviar = document.getElementById('enviar')
+
+const enviado = () => {
+innerHTML = value.nombre
+}
+
+enviar.addEventListener('click', enviado)
