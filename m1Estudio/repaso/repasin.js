b1f267d425d11648f4b3516108c22407942a8547
camilo Navas <navasochoa@hotmail.com>
2023-12-01 07:31:13 -0500

    ejercicios repaso m1

--- m1Estudio/repaso/repasin.js
+++ m1Estudio/repaso/repasin.js
@@ -53,3 +53,96 @@ let countProps = function(obj) {
     }
     return total
 }
+
+//Implementar el metodo changeNotNumbers dentro del prototype de LinkedList que deberá cambiar
+//aquellos valores que no puedes castearse a numeros 'Kiricocho' y devolver la cantidad de cambios que hizo
+//aclaracion: si el valor del nodo puede castearse a numero NO hay que reemplazarlo
+// ej1:
+// suponiendo que la lista actual es: Head --> [1] --> ['2'] --> [false] --> ['Franco]
+// lista.changeNotNumbers(
+// ahora la lista quedaría: Head --> [1] --> ['2'] --> [false] --> ['Kirikocho'] y 
+// la funcion deberia haber devuelto el valor 1
+
+LinkedList.prototype.changeNotNumbers = function() {
+    //poder revisar si se puede castear a numeros
+    //podemos revisar si se puede castear a numeros Number.isNaN(Number('Matias'))
+    //solo reemplazar los que no se pueden castear
+    //recorrer toda la LinkedList
+
+    let current = this.head
+    let sumar = 0
+    while (current) {
+        if (Number.isNaN(Number(current.value))) {
+            current.value = 'Kirikocho'
+            sumar++
+        }
+        current = current.next
+    }
+    return sumar
+}
+
+
+//Implementar la funcion mergeQueues que a apartir de dos queues recibidas por parametro
+//debe devolver una nueva Queue que vaya mergeando los nodos de las anteriores
+//ej:
+// - queueOne: [7,3,5]
+// - queueTwo: [2,4,6]
+// mergeQueues(queueOne, queueTow) --> [7,2,3,4,5,6]
+//IMPORTANTE: NO son arreglos sino que son Queues
+
+let mergeQueues = function(queueOne, queueTwo) {
+    let newQueue = new Queue()
+    while (queueOne.size() || queueTwo.size()) {
+        if (queueOne.size()) {
+        newQueue.enqueue(queueOne.dequeue())
+        }
+        if (queueTwo.size()) {
+            let value = queueTwo.dequeue()
+            newQueue.enqueue(value)
+        }
+    }
+    return newQueue
+}
+
+//Implementar la funcion closureMult que permita generar nuevas funciones representen
+//las tablas de multiplicacion de distintos numeros
+//ej:
+//- let mulByFour = closureMult(4)
+//- multByFour(2) --> 8 (2 * 4)
+//- multByFour(5) --> 20
+//- let multBySix = closureMult(6)
+//- multBySix(4) --> 24
+
+let clouseMult = function(multiplier) {
+    return function(num) {
+        return num * multiplier
+    }
+}
+
+//implementar el metodo sum dentro del prototype de BinarySearchTree
+//que debe retornar la suma total de los valores dentro de cada nodo del arbol
+
+BinarySearchTree.prototype.sum = function() {
+    let suma = this.value
+    if (this.left) {
+        suma += this.left.sum()
+    }
+    if (this.right) {
+        suma += this.right.sum()
+    }
+    return suma
+}
+
+
+function BinarioADecimal(num) {
+    let array = num.split('')
+    let num = 0
+    for (let i = 0; i < array.length; i++) {
+        num = num + parseInt(array[i]) * Math.pow(2, array.length -1 - i)
+    }
+    return num
+}
+
+function DecimalABinario(num) {
+    
+}
