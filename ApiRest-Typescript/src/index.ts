0021aaf45c2c3c04a60867826466d3f5f5b2e9d1
camilo Navas <navasochoa@hotmail.com>
2024-04-02 22:33:50 -0500

    typescript, api rest

---
+++ ApiRest-Typescript/src/index.ts
@@ -0,0 +1,18 @@
+import express, { Response } from 'express'
+import diaryRouter from './routes/diaries'
+
+const app = express()
+app.use(express.json())
+
+const PORT = 3000
+
+app.get('/ping', (_, res: Response) => {
+    console.log('someone pinged here!' + new Date().toLocaleDateString());
+    res.send('pong')
+})
+
+app.use('/api/diaries', diaryRouter)
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
---
+++ ejerciciosJs/hackerRank/4.diagonalDifference.js
@@ -0,0 +1,43 @@
+/*
+ ! Dada una matriz cuadrada, calcula la diferencia absoluta entre las sumas de sus diagonales
+ * Por ej:
+    * La matriz cuadrada arr se muestra a continuacion
+    *    1 2 3
+    *    4 5 6
+    *    9 8 9
+
+ ? La diagonal de izquierda a derecha = 1 + 5 + 9 = 15
+ ? La diagonal de derecha a izquierda = 3 + 5 + 9 = 17
+ ? Su diferencia absoluta es [15 - 17] = 2
+
+ ! Funcion descriptiva
+ * Completar la diagonalDifference funcion. Toma el siguiente parámetro:
+    * int arr[n][m]: una matriz de numeros enteros
+    ? DEVOLVER:
+        * int: la diferencia diagonal absoluta
+
+    ! Formato de entrada
+        * La primera linea contiene un unico numero entero, n, el numero de filas y columnas de la matriz cuadrada arr.
+        * Cada uno de los siguientes n lineas describe una fila, arr[i], y consta de n enteros separados por espacios arr[i][j]
+    
+
+    * RESTRICCIONES
+    ! -100 <= arr[i][j] <= 100
+
+
+    ! Formato de salida
+        *Devuelve la diferencia absoluta entre las sumas de las dos diagonales de la matriz como un unico numero entero
+        * EJs en HackerRank
+*/
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function diagonalDifference(arr) {
+    // Write your code here
+    
+}
